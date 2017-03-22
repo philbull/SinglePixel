14461e78009d4af0a79237d0c1534be0f48b99ea
Phil Bull <philbull@gmail.com>
2017-03-21 17:42:09 -0700

    Update models with more representative I,Q,U amplitudes, based on histograms of Planck FG maps smoothed to 1 deg and with 50pct mask

--- model_list.py
+++ model_list.py
@@ -2,29 +2,32 @@
 import models
 from utils import rj2cmb
 
+DUST_I = 50.
+DUST_P = 10. / 1.41
+
 # Define input models and their amplitudes/parameters
-dust_model = models.DustMBB( amp_I=rj2cmb(353e9, 150.), 
-                             amp_Q=rj2cmb(353e9, 10.), 
-                             amp_U=rj2cmb(353e9, 10.), 
+dust_model = models.DustMBB( amp_I=rj2cmb(353e9, DUST_I), 
+                             amp_Q=rj2cmb(353e9, DUST_P), 
+                             amp_U=rj2cmb(353e9, DUST_P), 
                              dust_beta=1.6, dust_T=20. )
 
-simple_dust_model = models.DustSimpleMBB( amp_I=rj2cmb(353e9, 150.), 
-                             amp_Q=rj2cmb(353e9, 10.), 
-                             amp_U=rj2cmb(353e9, 10.), 
+simple_dust_model = models.DustSimpleMBB( amp_I=rj2cmb(353e9, DUST_I), 
+                             amp_Q=rj2cmb(353e9, DUST_P), 
+                             amp_U=rj2cmb(353e9, DUST_P), 
                              dust_beta=1.6, dust_T=20. )
 
 simple_dust_model_shifted = models.DustSimpleMBB( 
-                             amp_I=rj2cmb(353e9, 150.), 
-                             amp_Q=rj2cmb(353e9, 10.), 
-                             amp_U=rj2cmb(353e9, 10.), 
+                             amp_I=rj2cmb(353e9, DUST_I), 
+                             amp_Q=rj2cmb(353e9, DUST_P), 
+                             amp_U=rj2cmb(353e9, DUST_P), 
                              dust_beta=1.7, dust_T=20. )
 
-ame_model = models.AMEModel( amp_I=rj2cmb(30e9, 80.), 
+ame_model = models.AMEModel( amp_I=rj2cmb(30e9, 30.), 
                              amp_Q=rj2cmb(30e9, 0.), 
                              amp_U=rj2cmb(30e9, 0.), 
                              nu_peak=25. )
 
-ff_model = models.FreeFreeUnpol( amp_I=rj2cmb(30e9, 80.), 
+ff_model = models.FreeFreeUnpol( amp_I=rj2cmb(30e9, 30.), 
                                  amp_Q=rj2cmb(30e9, 0.), 
                                  amp_U=rj2cmb(30e9, 0.), 
                                  ff_beta=-0.118 )
@@ -36,9 +39,9 @@ cmb_model = models.CMB( amp_I=50., amp_Q=0.6, amp_U=0.6 )
 
 # Silicate + Carbonaceous grains as 2 MBBs
 two_comp_silcar_model = models.DustGen( 
-                                     amp_I=rj2cmb(353e9, 150./1.25),
-                                     amp_Q=rj2cmb(353e9, 10./1.25),
-                                     amp_U=rj2cmb(353e9, 10./1.25),
+                                     amp_I=rj2cmb(353e9, DUST_I/1.25),
+                                     amp_Q=rj2cmb(353e9, DUST_P/1.25),
+                                     amp_U=rj2cmb(353e9, DUST_P/1.25),
                                      beta = 1.6,
                                      dbeta = 0.2,
                                      Td1 = 18.,
@@ -49,9 +52,9 @@ two_comp_silcar_model = models.DustGen(
 
 # Finkbeiner 1999 Model (should perhaps update fI,fQ,FQ to
 #                        representative values based on F99)
-two_comp_f99_model = models.DustGen( amp_I=rj2cmb(353e9, 150./1.2),
-                                     amp_Q=rj2cmb(353e9, 10./1.2),
-                                     amp_U=rj2cmb(353e9, 10./1.2),
+two_comp_f99_model = models.DustGen( amp_I=rj2cmb(353e9, DUST_I/1.2),
+                                     amp_Q=rj2cmb(353e9, DUST_P/1.2),
+                                     amp_U=rj2cmb(353e9, DUST_P/1.2),
                                      beta = 1.5,
                                      dbeta = 1.1,
                                      Td1 = 9.6,
@@ -65,9 +68,9 @@ two_comp_f99_model = models.DustGen( amp_I=rj2cmb(353e9, 150./1.2),
 # Magnetic field structure along the line of sight induces
 # depolarization, i.e. fQ != fU
 two_comp_cloud_model = models.DustGen( 
-                                     amp_I=rj2cmb(353e9, 150./2.),
-                                     amp_Q=rj2cmb(353e9, 10./2.1),
-                                     amp_U=rj2cmb(353e9, 10./1.8),
+                                     amp_I=rj2cmb(353e9, DUST_I/2.),
+                                     amp_Q=rj2cmb(353e9, DUST_P/2.1),
+                                     amp_U=rj2cmb(353e9, DUST_P/1.8),
                                      beta = 1.6,
                                      dbeta = 0.,
                                      Td1 = 20.,
@@ -78,9 +81,9 @@ two_comp_cloud_model = models.DustGen(
 
 # 2MBB Model with Fe grains
 # Note: polarized orthogonally to normal dust with beta = 0
-two_comp_fe_model = models.DustGen( amp_I=rj2cmb(353e9, 150./1.05),
-                                    amp_Q=rj2cmb(353e9, 10./0.95),
-                                    amp_U=rj2cmb(353e9, 10./0.95),
+two_comp_fe_model = models.DustGen( amp_I=rj2cmb(353e9, DUST_I/1.05),
+                                    amp_Q=rj2cmb(353e9, DUST_P/0.95),
+                                    amp_U=rj2cmb(353e9, DUST_P/0.95),
                                     beta = 1.6,
                                     dbeta = -1.6,
                                     Td1 = 20.,
@@ -90,17 +93,17 @@ two_comp_fe_model = models.DustGen( amp_I=rj2cmb(353e9, 150./1.05),
                                     fU = -0.05 )
 
 # HD17 Standard Model
-hd_model = models.DustHD( amp_I=rj2cmb(353e9, 150.),
-                          amp_Q=rj2cmb(353e9, 10.),
-                          amp_U=rj2cmb(353e9, 10.),
+hd_model = models.DustHD( amp_I=rj2cmb(353e9, DUST_I),
+                          amp_Q=rj2cmb(353e9, DUST_P),
+                          amp_U=rj2cmb(353e9, DUST_P),
                           fcar = 1.0,
                           fsilfe = 0.,
                           uval = 0.0 )
 
 # HD17 Model with Fe
-hd_fe_model = models.DustHD( amp_I=rj2cmb(353e9, 150.),
-                             amp_Q=rj2cmb(353e9, 10.),
-                             amp_U=rj2cmb(353e9, 10.),
+hd_fe_model = models.DustHD( amp_I=rj2cmb(353e9, DUST_I),
+                             amp_Q=rj2cmb(353e9, DUST_P),
+                             amp_U=rj2cmb(353e9, DUST_P),
                              fcar = 1.e3,
                              fsilfe = 1.e3,
                              uval = 0.0 )
